b0e01cbf97c1b7e2d684f218565b5e87285af725
Elizabeth H. Camp <betsy.h.camp@gmail.com>
2025-12-11 13:20:50 -0800

    feat: Add zero-filling to  initial_daily_taxi_rides.sql

--- queries/initial_daily_taxi_rides.sql
+++ queries/initial_daily_taxi_rides.sql
@@ -14,15 +14,15 @@ WITH max_pickup_date_cte AS (
 ),
 daily_timeseries_cte AS (
   SELECT 
-    pickup_taxi_zone_id,
-    pickup_date,
-    COUNT(trip_id) AS number_ride_pickups
+    trip.pickup_taxi_zone_id,
+    trip.pickup_date,
+    COUNT(trip.trip_id) AS number_ride_pickups
   FROM `nyc-taxi-ehc.curated.taxi_yellow_tripdata_fact` AS trip
   LEFT JOIN `nyc-taxi-ehc.curated.taxi_zone_dim` AS zone
   ON trip.pickup_taxi_zone_id = zone.taxi_zone_id
   WHERE zone.borough='Manhattan'
-    AND (trip.fare_amount !=0 OR trip.total_amount!=0)
-    AND (trip.trip_distance != 0 AND trip.total_amount <= 0)
+    AND NOT (trip.fare_amount =0 OR trip.total_amount=0)
+    AND NOT (trip.trip_distance = 0 AND trip.total_amount <= 0)
     AND pickup_date <= (
       SELECT max_pickup_date
       FROM max_pickup_date_cte
@@ -30,11 +30,28 @@ daily_timeseries_cte AS (
   GROUP BY 
     pickup_taxi_zone_id, 
     pickup_date
+),
+min_date_bound_cte AS (
+    SELECT
+        pickup_taxi_zone_id,
+        MIN(pickup_date) AS min_pickup_date
+    FROM daily_timeseries_cte
+    GROUP BY pickup_taxi_zone_id
+),
+pickup_taxi_zone_id_cal AS (
+    SELECT
+        mn.pickup_taxi_zone_id,
+        d.calendar_date
+    FROM min_date_bound_cte AS mn
+    CROSS JOIN max_pickup_date_cte AS mx
+    JOIN `nyc-taxi-ehc.curated.date_dim` AS d
+    ON d.calendar_date >= mn.min_pickup_date
+    AND d.calendar_date <= mx.max_pickup_date
 )
+
 SELECT
-  t.pickup_taxi_zone_id,
-  t.pickup_date,
-  t.number_ride_pickups,
+  p.pickup_taxi_zone_id,
+  p.calendar_date AS pickup_date,
   d.calendar_year,
   d.calendar_month,
   d.day_of_week,
@@ -42,7 +59,14 @@ SELECT
   d.is_weekend,
   d.is_holiday,
   d.holiday_name,
-  d.is_daylight_savings
-FROM daily_timeseries_cte AS t
+  d.is_daylight_savings,
+  COALESCE(t.number_ride_pickups, 0) AS number_ride_pickups
+FROM pickup_taxi_zone_id_cal AS p
+LEFT JOIN daily_timeseries_cte AS t
+    ON p.calendar_date = t.pickup_date
+    AND p.pickup_taxi_zone_id = t.pickup_taxi_zone_id
 LEFT JOIN `nyc-taxi-ehc.curated.date_dim` AS d
-ON t.pickup_date = d.calendar_date
+ON p.calendar_date = d.calendar_date
+ORDER BY 
+    p.pickup_taxi_zone_id, 
+    p.calendar_date
