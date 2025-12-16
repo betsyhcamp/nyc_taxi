5bca5b9960dbf40bce8b26157087d9385341878b
Elizabeth H. Camp <betsy.h.camp@gmail.com>
2025-12-15 23:08:11 -0800

    WIP: refac  extract_db_to_bucket.py to have read_sql, root_dir finding outside component

--- src/fcstnyctaxi/core/feature/extract_db_to_bucket.py
+++ src/fcstnyctaxi/core/feature/extract_db_to_bucket.py
@@ -15,23 +15,27 @@ def extract_db_to_bucket(
     db_location: str,
     bucket_name: str,
     prefix: str,
-    query_filename: str,
+    sql_query_str: str,
 ) -> str:
-    project_root_path = get_project_root_dir()
-
-    sql = read_sql(project_root_path / "queries" / query_filename)
+    # put in pipeline file
+    # project_root_path = get_project_root_dir()
+    # put in pipeline file
+    # sql = read_sql(project_root_path / "queries" / query_filename)
 
     # run BQ
     bq_client = bigquery.Client(project=project_id, location=db_location)
-    job = bq_client.query(sql)
+    job = bq_client.query(sql_query_str)
     print("Billed project (job.project):", job.project)
     df = job.result().to_dataframe()
+
+    # consider later transfering queried data directly to GCS rather than
+    # going through pandas and a local write
 
     # write to local .parquet file w/ string in the filename: UUID_utctimenow
     dataset_id = uuid.uuid4().hex[:8]
     timestamp = datetime.now(timezone.utc).strftime("UTC%Y%m%d_%H%M%S")
     filename = f"manhattan_daily_zone_pickups_{dataset_id}_UTC{timestamp}.parquet"
-    tmp_dir = project_root_path / "tmp_data"
+    tmp_dir = Path("/tmp/data")
     tmp_dir.mkdir(parents=False, exist_ok=True)
     local_path = tmp_dir / filename
     df.to_parquet(local_path, index=False)
@@ -43,6 +47,7 @@ def extract_db_to_bucket(
     blob = bucket.blob(f"{prefix}/{filename}")
     blob.upload_from_filename(local_path)
 
+    # TO DO: any print statement should be part of logging
     print(f"Wrote {len(df)} rows to {gcs_uri}")
 
     return gcs_uri
@@ -63,10 +68,17 @@ if __name__ == "__main__":
     gcs_prefix = configs["extract_db_to_bucket"].get("gcs_prefix")
     gcs_prefix = env + "/" + gcs_prefix
 
+    query_filename = (configs["extract_db_to_bucket"].get("query_filename"),)
+    sql_query_str = read_sql(project_root_path / "queries" / query_filename)
+    # put in pipeline file
+    # project_root_path = get_project_root_dir()
+    # put in pipeline file
+    # sql = read_sql(project_root_path / "queries" / query_filename)
+
     extract_db_to_bucket(
         project_id=configs["project_settings"].get("project_id"),
         db_location=configs["project_settings"].get("location"),
         bucket_name=configs["project_settings"].get("gcs_bucket_name"),
         prefix=gcs_prefix,
-        query_filename=configs["extract_db_to_bucket"].get("query_filename"),
+        sql_query_str=sql_query_str,
     )
